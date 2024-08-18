416ec1c942035e9f152be9084c152007a81ef21e
Murilo Assis <Muassis38@gmail.com>
2024-08-18 18:45:43 -0300

    Aprendizado de metodos

--- java-anatomia-de-classes/src/MinhaClasse.java
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -1,9 +1,15 @@
 public class MinhaClasse{
 
 public static void main(String [] args){
-    System.out.print("Ola");
-    final String BR = "Brasil";
-    
+    String primeiroNome = "Murilo";
+    String segundoNome = "Assis";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);;
     }
+public static String nomeCompleto(String primeiroNome, String segundoNome){
+    return primeiroNome.concat(" ").concat(segundoNome);
+}
 
 }

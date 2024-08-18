b9cfeb0c7d4188934b3da8b0897d868061954756
Murilo Assis <Muassis38@gmail.com>
2024-08-18 19:24:36 -0300

    Sitemas de boletim Basico, para aprendizagem de identação e organização de arquivos

---
+++ java-anatomia-de-classes/src/edu/murilo/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+package edu.murilo.primeirasemana;
+
+public class BoletimEstudantil {
+    
+    public static void main(String[] args) {
+        int mediaFinal = 5;
+        if (mediaFinal < 6) {
+            System.out.println("Reprovado");
+        }else if (mediaFinal == 6) {
+            System.out.println("Prova Final");
+        }else{
+            System.out.println("Aprovado");
+        }
+    }
+}
